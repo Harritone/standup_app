5fcfd02345ef1edffbacaedacf3374fb49c64c0e
Hariton <yourrasik@gmail.com>
2022-02-06 10:26:18 +0000

    Add support page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'support', to: 'static#support'
   get 'activity/mine'
   get 'activity/feed'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
